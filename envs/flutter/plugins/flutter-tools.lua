4544cdbdb951f0790ca5720ff266bbedfcf6bf20
jae_macair <jae_k@hotmail.com>
2024-12-20 13:47:32 +0900

    flutter envs

---
+++ envs/flutter/plugins/flutter-tools.lua
@@ -0,0 +1,15 @@
+print("loading flutter-tools")
+
+return {
+	{
+		"nvim-flutter/flutter-tools.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"stevearc/dressing.nvim", -- optional for vim.ui.select
+		},
+		config = function()
+			require("flutter-tools").setup({})
+		end,
+	},
+}
