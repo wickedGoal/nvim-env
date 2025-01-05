a4982b04afd7b8c8e801f70ee5e592004c0f2329
jae_desktop <jae_k@hotmail.com>
2025-01-05 16:55:21 +0900

    add mason completion

--- envs/mason/plugins/mason.lua
+++ envs/mason/plugins/mason.lua
@@ -18,6 +18,25 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			require("lspconfig").lua_ls.setup({})
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if not client then
+						return
+					end
+
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							buffer = args.buf,
+							callback = function()
+								-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+								vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+							end,
+						})
+					end
+				end,
+			})
 		end,
 	},
 }
