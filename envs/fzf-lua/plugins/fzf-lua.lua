c197010c7aeb7c10fb583a3c418d2d320352ded0
jae_desktop <jae_k@hotmail.com>
2025-01-05 16:20:34 +0900

    add keys to fzf-lua

--- envs/fzf-lua/plugins/fzf-lua.lua
+++ envs/fzf-lua/plugins/fzf-lua.lua
@@ -1,6 +1,12 @@
 return {
 	{
 		"ibhagwan/fzf-lua",
+		keys = {
+			{ "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>", desc = "Files" },
+			{ "<leader>fg", "<cmd>lua require('fzf-lua').live_grep()<CR>", desc = "Live Grep" },
+			{ "<leader>fh", "<cmd>lua require('fzf-lua').helptags()<CR>", desc = "Help Tags" },
+			{ "<leader>fm", "<cmd>lua require('fzf-lua').manpages()<CR>", desc = "Man Pages" },
+		},
 		-- optional for icon support
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
