bc3bd002aaf41c222db968b8285c234f39457c8f
jae_laptop <jae_k@hotmail.com>
2025-01-05 11:28:16 +0900

    fix mason load lua

--- envs/mason/plugins/mason.lua
+++ envs/mason/plugins/mason.lua
@@ -4,12 +4,20 @@ return {
 		cmd = "Mason",
 		keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
 		build = ":MasonUpdate",
+		config = function()
+			require("mason").setup()
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		config = function() end,
+		config = function()
+			require("mason-lspconfig").setup()
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
+		config = function()
+			require("lspconfig").lua_ls.setup({})
+		end,
 	},
 }
