2bcd1f3d6de034605cc2998ac58e9f3ed6bb32af
jae_desktop <jae_k@hotmail.com>
2025-01-05 16:33:39 +0900

    add keys to fzf-lua configs and packages

--- envs/fzf-lua/plugins/fzf-lua.lua
+++ envs/fzf-lua/plugins/fzf-lua.lua
@@ -6,6 +6,16 @@ return {
 			{ "<leader>fg", "<cmd>lua require('fzf-lua').live_grep()<CR>", desc = "Live Grep" },
 			{ "<leader>fh", "<cmd>lua require('fzf-lua').helptags()<CR>", desc = "Help Tags" },
 			{ "<leader>fm", "<cmd>lua require('fzf-lua').manpages()<CR>", desc = "Man Pages" },
+			{
+				"<leader>fc",
+				"<cmd>lua require('fzf-lua').files({cwd = vim.fn.stdpath('config')})<CR>",
+				desc = "Config",
+			},
+			{
+				"<leader>fp",
+				"<cmd>lua require('fzf-lua').files({cwd = vim.fs.joinpath(vim.fn.stdpath('data'), 'lazy')})<CR>",
+				desc = "Packages",
+			},
 		},
 		-- optional for icon support
 		dependencies = { "nvim-tree/nvim-web-devicons" },
